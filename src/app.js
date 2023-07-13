5103bede644a33485fab5b513f8e21dc2533f29a
Dayane <dayanebmartins@gmail.com>
2023-07-13 18:41:00 -0300

    feat(rotas): Adiciona a rota pets no arquivo app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const petsRoutes = require('./routes/petsRoutes');
 
 const app = express();
+app.use(express.json());
+
+app.use('/pets', petsRoutes);
 
 module.exports = app;
