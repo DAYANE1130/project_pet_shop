f2286ed3e7db4c449eee27d032e49231b2af597e
Dayane <dayanebmartins@gmail.com>
2023-07-13 18:46:28 -0300

    feat(conexao com banco de dados): corrige variveis de ambiente

---
+++ src/models/connection.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const connection = mysql.createPool({
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
+  database: 'pet_shop',
+});
+
+module.exports = connection;
