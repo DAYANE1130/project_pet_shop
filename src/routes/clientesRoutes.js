e0f03b10659d51ed85d753992bfe1db6b0435584
Dayane <dayanebmartins@gmail.com>
2023-07-13 18:58:46 -0300

    fix(rotas): corrige parametros de rota em clientesRoutes

--- src/routes/clientesRoutes.js
+++ src/routes/clientesRoutes.js
@@ -1,9 +1,7 @@
 const route = require('express').Router();
 
-route.post('/cliente');
-route.get('/cliente');
-route.get('/cliente/:id');
-route.put('/cliente/:id');
-route.delete('/cliente/:id');
-
-module.exports = route;
+route.post('/');
+route.get('/');
+route.get('/:id');
+route.put('/:id');
+route.delete('/:id');
