ed7446923a33eb503ddb2c6fc9e6175afb0a9868
Dayane <dayanebmartins@gmail.com>
2023-07-13 19:14:59 -0300

    feat(Rotas): acrescenta a linha que possibilita exporta a rota de clientes module.exports

--- src/routes/clientesRoutes.js
+++ src/routes/clientesRoutes.js
@@ -1,7 +1,10 @@
 const route = require('express').Router();
+// const petsControllers = require('../controllers/petsController');
 
 route.post('/');
 route.get('/');
 route.get('/:id');
 route.put('/:id');
 route.delete('/:id');
+
+module.exports = route;
