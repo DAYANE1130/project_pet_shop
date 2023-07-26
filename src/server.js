99783385d1274e375ccddf704eca2c0485a0d7e6
Dayane <dayanebmartins@gmail.com>
2023-07-26 17:48:33 -0300

    chore(Servidor- app.js e server.js):
    corrige nome do arquivo

--- src/server.js
+++ src/server.js
@@ -4,3 +4,12 @@ const app = require('./app');
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// import dotenv from 'dotenv';
+// import app from './app';
+
+// dotenv.config();
+
+// const PORT = process.env.PORT || 3001;
+
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
