c4380aecc4a5925108effb813de09be378f4c92d
Dayane <dayanebmartins@gmail.com>
2023-07-26 17:51:49 -0300

    chore(Servidor- app.js e server.js):
    Instação do pacote  de tratamento de erros  do express

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 const express = require('express');
+require('express-async-errors');
 const petsRoutes = require('./routes/petsRoutes');
 const clientesRoutes = require('./routes/clientesRoutes');
+
+// const INTERNAL_SERVER_ERROR = 500;
 
 const app = express();
 app.use(express.json());
 
 app.use('/pets', petsRoutes);
 app.use('/clientes', clientesRoutes);
+
+app.use((error, req, res, _next) => {
+  const status = error.status || 500;
+  const message = 'Erro inesperado, tente mais tarde';
+  console.error(error);
+  res.status(status).json({ error: message });
+});
 
 module.exports = app;
