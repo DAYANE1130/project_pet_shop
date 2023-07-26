cbf9b435969bad28f1f26b94422315779fb5b1e5
Dayane <dayanebmartins@gmail.com>
2023-07-26 18:12:48 -0300

    feat(middlewareError): Cria middleware de erro e pasta para gerenciar erros da aplicação

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('express-async-errors');
 const petsRoutes = require('./routes/petsRoutes');
 const clientesRoutes = require('./routes/clientesRoutes');
+const errorMiddleware = require('./middlewares/error');
 
 // const INTERNAL_SERVER_ERROR = 500;
 
@@ -11,11 +12,6 @@ app.use(express.json());
 app.use('/pets', petsRoutes);
 app.use('/clientes', clientesRoutes);
 
-app.use((error, req, res, _next) => {
-  const status = error.status || 500;
-  const message = 'Erro inesperado, tente mais tarde';
-  console.error(error);
-  res.status(status).json({ error: message });
-});
+app.use(errorMiddleware);
 
 module.exports = app;
---
+++ src/middlewares/error.js
@@ -0,0 +1,8 @@
+const error = (err, req, res, _next) => {
+  const status = err.status || 500;
+  const message = err.message || 'Erro inesperado, tente mais tarde';
+  console.error(err);
+  res.status(status).json({ error: message });
+};
+
+module.exports = error;
