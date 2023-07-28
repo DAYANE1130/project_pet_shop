99b286dc439fa225a4161e03d66936f80aaeead7
Dayane <dayanebmartins@gmail.com>
2023-07-28 15:06:43 -0300

    feat(Camada controller):
    cria função getById

--- src/controllers/petsControllers.js
+++ src/controllers/petsControllers.js
@@ -1,10 +1,18 @@
 const petsServices = require('../services/petsServices');
 
 const OK = 200;
+const NOTFOUND = 404;
 
 const getAll = async (req, res) => {
   const pets = await petsServices.getAll();
   res.status(OK).json(pets);
 };
 
-module.exports = { getAll };
+const getById = async (req, res) => {
+  const { id } = req.params; // cliente envia pela url da pesquisa
+  const pet = await petsServices.getById(id);
+  if (!pet) return res.status(NOTFOUND).json({ message: 'Pet not found' });
+  return res.status(OK).json(pet);
+};
+
+module.exports = { getAll, getById };
