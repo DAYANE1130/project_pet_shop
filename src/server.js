8ecc839f54c3bac34800fc32050eb536cd60b96a
Dayane <dayanebmartins@gmail.com>
2023-07-28 15:04:58 -0300

    chore(Servidor- app.js e server.js):
    retira comentário desnecessário do arquivo server

--- src/server.js
+++ src/server.js
@@ -4,12 +4,3 @@ const app = require('./app');
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-// import dotenv from 'dotenv';
-// import app from './app';
-
-// dotenv.config();
-
-// const PORT = process.env.PORT || 3001;
-
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
