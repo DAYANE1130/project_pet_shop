c99b1d2935aae414f07dca5e2827dd29e7c8c3bc
Dayane <dayanebmartins@gmail.com>
2023-07-28 21:13:13 -0300

    feat(Rotas):
    cria rota que permite deletar um pet

--- src/routes/petsRoutes.js
+++ src/routes/petsRoutes.js
@@ -1,10 +1,10 @@
 const route = require('express').Router();
 const petsControllers = require('../controllers/petsControllers');
 
-route.post('/');
-route.get('/', petsControllers.getAll);
-route.get('/:id', petsControllers.getById); // conferir
-route.put('/:id');
-route.delete('/:id');
+route.post('/'); // FALTA ESSA
+route.get('/pets/', petsControllers.getAll);
+route.get('/pet/:id', petsControllers.getById); 
+route.put('/pet/:id', petsControllers.update);
+route.delete('/pet/:id', petsControllers.remove);
 
 module.exports = route;
