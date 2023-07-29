6607a4dfa15611c4b3d39337b3fd68dc786e82d5
Dayane <dayanebmartins@gmail.com>
2023-07-28 21:12:32 -0300

    feat(Camada models):
    Cria função que permite deletar um pet

--- src/models/petsModels.js
+++ src/models/petsModels.js
@@ -9,14 +9,19 @@ const getAll = async () => {
 const getById = async (id) => {
   const query = 'SELECT * FROM pet_shop.pets WHERE id=?';
   const [[pet]] = await connection.execute(query, [id]);
-  console.log(pet);
   return pet;
 };
 
 const update = async (id, nome) => {
   const query = 'UPDATE  pet_shop.pets SET nome=? WHERE id=?';
-  const [petUpdated] = await connection.execute(query, [id, nome]);
+  const [petUpdated] = await connection.execute(query, [nome, id]);
   return { id: petUpdated.insertId, nome };
 };
 
-module.exports = { getAll, getById, update };
+const remove = async (id) => {
+  const query = 'DELETE FROM pet_shop.pets WHERE id=?';
+  const [petDeleted] = await connection.execute(query, [id]);
+  return petDeleted;
+};
+
+module.exports = { getAll, getById, update, remove };
