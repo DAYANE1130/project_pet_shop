093bbf9ad468b3d43a6cfe0e3bbdd7b0938c8b79
Dayane <dayanebmartins@gmail.com>
2023-08-04 20:08:39 -0300

    chore(Configuracao Swagger):

--- src/swagger.js
+++ src/swagger.js
@@ -1,7 +1,7 @@
 const swaggerAutogen = require('swagger-autogen')();
 
-const outputFile = './swagger_output.json'; 
+const outputFile = './swagger_output.json';
 // terá o arquivo de configuração sobre nossas rotas que estão rodando no Express.
-const endpointsFiles = ['./routes/petsRoutes'];
+const endpointsFiles = ['./routes/petsRoutes', './routes/donosRoutes'];
 // é o nosso caminho para as rotas a serem documentadas.
 swaggerAutogen(outputFile, endpointsFiles);
