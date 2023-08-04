48162f138121d499f2bc9d7ba16902448cbacd55
Dayane <dayanebmartins@gmail.com>
2023-08-04 20:07:02 -0300

    feat(Funcoes complementares):
    Cria função  que organiza a resposta dos dados dos clientes em um objeto com dados pessoais e endereço

--- src/utils.js
+++ src/utils.js
@@ -7,4 +7,14 @@ const getKeysAndValues = async (body) => {
   return dataBody;
 };
 
-module.exports = getKeysAndValues;
+const responseFormatedDono = async (array) => {
+  const dataFormated = array.map((element) => {
+    const { id, nome, email, telefone, rua, numero, cidade, estado, cep } = element;
+    const personalData = { id, nome, email, telefone };
+    const address = { rua, numero, cidade, estado, cep };
+    return { ...personalData, address };
+  });
+  return dataFormated;
+};
+
+module.exports = { getKeysAndValues, responseFormatedDono };
