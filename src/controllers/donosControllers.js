1edc12c573e0bca48c21675edca8b4949e8f4b22
Dayane <dayanebmartins@gmail.com>
2023-08-07 11:11:34 -0300

    feat(Camada controller):
    Cria função que permite atualizar um dono

--- src/controllers/donosControllers.js
+++ src/controllers/donosControllers.js
@@ -1,7 +1,7 @@
 const donosServices = require('../services/donosServices');
 
 const OK = 200;
- const NOTFOUND = 404;
+const NOTFOUND = 404;
 
 const create = async (req, res) => {
   const data = req.body;
@@ -19,13 +19,20 @@ const getById = async (req, res) => {
   const dono = await donosServices.getById(id);
   if (!dono) return res.status(NOTFOUND).json({ message: 'Dono not found' });
   return res.status(OK).json(dono);
-  };
+};
 
-  const remove = async (req, res) => {
-    const { id } = req.params;
-    const donoDeleted = await donosServices.remove(Number(id));
-    if (!donoDeleted) return res.status(NOTFOUND).json({ message: 'Dono not found' });
-    return res.status(OK).json({ message: 'Dono successfully deleted' });
-    };
-    
-module.exports = { create, getAll, getById, remove };
+const update = async (req, res) => {
+  const { id } = req.params;
+  const donoUpdated = await donosServices.update(id, req.body);
+  if (!donoUpdated) return res.status(NOTFOUND).json({ message: 'Dono not found' });
+  res.status(OK).json(donoUpdated);
+};
+
+const remove = async (req, res) => {
+  const { id } = req.params;
+  const donoDeleted = await donosServices.remove(Number(id));
+  if (!donoDeleted) return res.status(NOTFOUND).json({ message: 'Dono not found' });
+  return res.status(OK).json({ message: 'Dono successfully deleted' });
+};
+
+module.exports = { create, getAll, getById, update, remove };
