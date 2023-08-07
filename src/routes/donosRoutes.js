75fea75c999a3df22427ffa8335a18de0cd4ded9
Dayane <dayanebmartins@gmail.com>
2023-08-07 11:12:08 -0300

    feat(Rotas):
    Cria rota que permite atualizar um dono

--- src/routes/donosRoutes.js
+++ src/routes/donosRoutes.js
@@ -4,7 +4,7 @@ const donosControllers = require('../controllers/donosControllers');
 route.post('/dono', donosControllers.create);
 route.get('/donos/', donosControllers.getAll);
 route.get('/dono/:id', donosControllers.getById);
-route.put('/:id');
+route.patch('/dono/:id', donosControllers.update);
 route.delete('/dono/:id', donosControllers.remove);
 
 module.exports = route;
