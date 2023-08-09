be759ff52dd3d130c11fbbc15c32114f8499907f
Dayane <dayanebmartins@gmail.com>
2023-08-09 15:52:27 -0300

    refactor(Camada controller):
    Refatora o res.status para  usar biblioteca statusCodes

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,10 +1,9 @@
+const { StatusCodes } = require('http-status-codes');
 const userServices = require('../services/usersServices');
-
-const OK = 200;
 
 const create = async (req, res) => {
   const user = await userServices.create(req.body);
-  res.status(OK).json(user);
+  res.status(StatusCodes.OK).json(user);
 };
 
 module.exports = { create };
