ab7c952f2b07e0532d8fefcc32b9a89eee171016
Dayane <dayanebmartins@gmail.com>
2023-08-09 15:50:22 -0300

    refactor(MiddlewareError):
    Muda string que identifca o case  de maiuscula para minuscula dentro do switch case

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -3,7 +3,7 @@ const { StatusCodes } = require('http-status-codes');
 const errorHandler = (err, _req, res, _next) => {
   const { status, message } = err;
   switch (status) {
-    case 'NOTFOUND': res.status(StatusCodes.NOT_FOUND).json({ message });
+    case 'Not found': res.status(StatusCodes.NOT_FOUND).json({ message });
     break;
     default: res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message });
   }
