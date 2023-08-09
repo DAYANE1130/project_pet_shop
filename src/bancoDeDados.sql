3c7b369766542afc52064de23512fab6ffab5a9a
Dayane <dayanebmartins@gmail.com>
2023-08-09 15:17:15 -0300

    refactor(Tabela banco de dados):
    Cria nova tabela  users e insere as colunas firts e last name

--- src/bancoDeDados.sql
+++ src/bancoDeDados.sql
@@ -7,7 +7,8 @@ USE pet_shop;
 CREATE TABLE
     users(
         id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        fullName VARCHAR(100) NOT NULL,
+        first_name VARCHAR(50) NOT NULL,
+        last_name VARCHAR(50) NOT NULL,
         email VARCHAR(50) NOT NULL,
         password VARCHAR(50) NOT NULL,
         created_at datetime,
