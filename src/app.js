ac8650cbaa6be0af4d30844e95fdf4f068de4f8c
Dayane <dayanebmartins@gmail.com>
2023-08-10 14:31:02 -0300

    refactor(Servidor- app.js e server.js):
    coloquei o nome do arquivo de erro no plural e corrigi importação do mesmo

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ require('express-async-errors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger_output.json');
 const routes = require('./routes/index');
-const errorMiddleware = require('./middlewares/error');
+const errorMiddleware = require('./middlewares/errors');
 
 const app = express();
 app.use(express.json());
