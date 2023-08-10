c7fa7facd0b84d2877b3aea87ee2633810065e9d
Dayane <dayanebmartins@gmail.com>
2023-08-10 14:39:35 -0300

    feat(Rotas):
    Adiciona validações para criar um novo usuário

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,7 +1,12 @@
 const route = require('express').Router();
 
 const usersControllers = require('../controllers/usersControllers');
+const validateUsers = require('../middlewares/validateUsers');
 
-route.post('/user', usersControllers.create);
+route.post('/user',
+  validateUsers.validateFirstLastName,
+  validateUsers.validateEmail,
+  validateUsers.validatePassword,
+  usersControllers.create);
 
 module.exports = route;
