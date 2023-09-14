1d1d7c3eaef497645a6c33250f3a6af47aa0eef0
Dayane <dayanebmartins@gmail.com>
2023-09-14 13:11:29 -0300

    feat(validacoes):
    Cria validações para gerenciamento dos dados da tabela pets

--- src/middlewares/validatePets.js
+++ src/middlewares/validatePets.js
@@ -1,19 +1,51 @@
 const { StatusCodes } = require('http-status-codes');
+const { verifyDataIsString, verifyFieldsIsNotBlank, verifyDataIsNumber } = require('../utils');
 
 const errors = {
-donoIdOrIdadeIsNotValid: '"dono_id"  must be a number',
-  tipoIsNotValid: '"tipo" must be a valid ',
-  nomeIsNotValid: '"nome" must be a valid',
-  idadeNotValid: '"idade" must be a number',
-  racaNotValid: '"raca" must be a valid',
+  fieldsIsNotStrings: 'fields  must be a strings',
+  fieldsIsNotNumbers: '"dono_id" and "idade" must be a number  ',
+  fieldsIsBlank: 'Attributes are missing',
+  updateFieldsIsBlank: ' Update Attributes  are missing',
 };
 
-const validateDonoId = (req, _res, next) => {
-  const { dono_id: donoId } = req.body;
-  if (typeof donoId !== 'number') {
-    return next({ status: StatusCodes.BAD_REQUEST, message: errors.firstNameOrLastNameIsNotValid });
+const validateFields = (req, _res, next) => {
+  const { dono_id: donoId, tipo, nome, idade, raca } = req.body;
+ 
+  const fields = [donoId, tipo, nome, idade, raca];
+  const values = [...Object.values(req.body)];
+
+  if (!verifyFieldsIsNotBlank(fields)) {
+    return next({ status: StatusCodes.BAD_REQUEST, message: errors.fieldsIsBlank });
+  }
+  
+  if (!verifyFieldsIsNotBlank(values)) {
+    return next({ status: StatusCodes.BAD_REQUEST, message: errors.updateFieldsIsBlank });
+  }
+  next();
+};
+ // assim funciona
+const validateFieldsUpdate = (req, _res, next) => {
+  const values = [...Object.values(req.body)];
+
+ // console.log(values)
+
+  if (!verifyFieldsIsNotBlank(values)) {
+    return next({ status: StatusCodes.BAD_REQUEST, message: errors.fieldsIsBlank });
   }
   next();
 };
 
-module.exports = { validateDonoId };
+const validateDataType = (req, _res, next) => {
+  const { tipo, nome, raca, dono_id: donoId, idade } = req.body;
+  const fieldsStrings = [tipo, nome, raca];
+  const fieldsNumbers = [donoId, idade];
+  if (!verifyDataIsString(fieldsStrings)) {
+    return next({ status: StatusCodes.BAD_REQUEST, message: errors.fieldsIsNotStrings });
+  }
+  if (!verifyDataIsNumber(fieldsNumbers)) {
+    return next({ status: StatusCodes.BAD_REQUEST, message: errors.fieldsIsNotNumbers });
+  }
+  next();
+};
+
+module.exports = { validateFields, validateFieldsUpdate, validateDataType };
