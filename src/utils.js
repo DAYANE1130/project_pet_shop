96e9654babb936225ea232952b7fdbe483fd78e2
Dayane <dayanebmartins@gmail.com>
2023-09-14 13:07:00 -0300

    feat(validacoes):
    Adiciona funções genericas para validar  se o  tipo de dado está correto

--- src/utils.js
+++ src/utils.js
@@ -17,4 +17,38 @@ const responseFormatedDono = async (array) => {
   return dataFormated;
 };
 
-module.exports = { getKeysAndValues, responseFormatedDono };
+const verifyFormatEmail = (email) => {
+  const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  return regex.test(email);
+};
+
+const verifyPasswordStrength = (password) => {
+  const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()])/;
+  if (passwordPattern.test(password) === false) return false;
+  return true;
+};
+
+const verifyDataIsString = (data) => {
+  const verifyFieldsString = data.every((field) => typeof field === 'string');
+  return verifyFieldsString;
+};
+
+const verifyDataIsNumber = (data) => {
+  const verifyFieldsNumbers = data.every((field) => typeof field === 'number');
+  return verifyFieldsNumbers;
+};
+
+const verifyFieldsIsNotBlank = (data) => {
+  const fields = data.every((field) => field);
+  return fields;
+};
+
+module.exports = {
+  getKeysAndValues,
+  responseFormatedDono,
+  verifyFormatEmail,
+  verifyPasswordStrength,
+  verifyDataIsString,
+  verifyDataIsNumber,
+  verifyFieldsIsNotBlank,
+};
