49796406b08fe7b43d61d41be29a1a515f68f696
Dayane <dayanebmartins@gmail.com>
2024-02-26 18:33:02 -0300

    feat(Rotas):
    Insere função que compara senha do usuario com a senha do db com bcrypt

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -6,11 +6,9 @@ const loginUser = async (req, res, next) => {
 
   const user = await loginService.loginUser(email, password);
   const { errorPassword } = user;
-  // tratar erro
-  // validando se o usuario enviar um -email que não existe
   if (!user) return next({ status: StatusCodes.BAD_REQUEST, message: 'User not found' });
   if (errorPassword) return next({ status: StatusCodes.BAD_REQUEST, message: 'Error password' });
-  return res.status(StatusCodes.OK).json(user);
+  return res.status(StatusCodes.OK).json({ token: user });
 };
 
 module.exports = { loginUser };
--- src/services/loginServices.js
+++ src/services/loginServices.js
@@ -1,17 +1,21 @@
 const loginModels = require('../models/loginModels');
+const comparePassword = require('./comparePassword');
+const jwt = require('../utils/auth');
 
 const loginUser = async (email, password) => {
-  if (!email || !password) return false; // acho que isso deveria vir na validação
-  // acho que a linha acima garante que vou consultar só se enviar algum dado , certo ?
-  const user = await loginModels.loginUser(email); // usar find, isso é um array ?
-  if (!user) return false;
-  // a senha que o usuário enviou confere com a cadastrada no banco de dados ?
-  // Não é necessário usar find para procurarmos , a consulta fpoi feita usando "where email ="
-  // então vai retornar somente 1 user
-  // O método find() retorna o valor do primeiro elemento do array que satisfizer 
-  if (user.password !== password) return { errorPassword: 'error' };
+  if (!email || !password) return false;
 
-  return user; // retornando user paar efeito de teste , vou retornar um token JWT
+  const user = await loginModels.loginUser(email);
+
+  if (!user) return false;
+
+  const compare = comparePassword.comparePassword(password, user.password);
+
+  if (!compare) return { errorPassword: 'error' };
+
+  const token = jwt.generateToken(email);
+
+  return token;
 };
 
 module.exports = { loginUser };
