a32c5443dbef79bf828f22192149d2c99a0efa79
Dayane <dayanebmartins@gmail.com>
2024-02-26 18:25:14 -0300

    refactor(Tabela banco de dados):
    aumenta o numero de caracteres da senha

--- src/bancoDeDados.sql
+++ src/bancoDeDados.sql
@@ -10,7 +10,7 @@ CREATE TABLE
         first_name VARCHAR(50) NOT NULL,
         last_name VARCHAR(50) NOT NULL,
         email VARCHAR(50) NOT NULL,
-        password VARCHAR(50) NOT NULL,
+        password VARCHAR(100) NOT NULL,
         created_at datetime,
         updated_at datetime
     
