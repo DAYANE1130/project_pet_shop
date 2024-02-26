fb867ae29a9668c089856ba09a290a49fb2f3190
Dayane <dayanebmartins@gmail.com>
2024-02-26 18:29:25 -0300

    feat(Camada service):
    Implementa hash da senha ao criar um  usuario

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -3,7 +3,7 @@ const userServices = require('../services/usersServices');
 
 const create = async (req, res) => {
   const user = await userServices.create(req.body);
-  res.status(StatusCodes.OK).json(user);
+  res.status(StatusCodes.OK).json({ status: 'Sucessfull user', user });
 };
 
 module.exports = { create };
