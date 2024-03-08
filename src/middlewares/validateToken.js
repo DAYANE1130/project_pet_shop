ed83dc34f7015a116726df351ddc0907a724d0b0
Dayane <dayanebmartins@gmail.com>
2024-03-08 15:30:43 -0300

    feat(Middleware de Validacoes):
    Cria função que valida um token

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,19 +1,19 @@
 const jwt = require('jsonwebtoken');
 const { StatusCodes } = require('http-status-codes');
 
-const validateToken = async (req, res, next) => {
+const verifyToken = async (req, res, next) => {
   const authHeader = req.headers.authorization;
   const token = authHeader.split(' ')[1];
   const secret = process.env.JWT_SECRET;
 
-  if (!token) return res.status({ status: StatusCodes.UNAUTHORIZED, message: 'Token not found' });
+  if (!token) return next({ status: StatusCodes.UNAUTHORIZED, message: 'Token not found' });
   try {
     const decodedToken = jwt.verify(token, secret);
     req.payload = decodedToken;
     next();
   } catch (error) {
-    return res.status({ status: StatusCodes.UNAUTHORIZED, message: 'Expired or invalid token' });
+    return next({ status: StatusCodes.UNAUTHORIZED, message: 'Expired or invalid token' });
   }
 };
 
-module.exports = { validateToken };
+module.exports = { verifyToken };
