064ced92c1e600ab81b2b1d63c3fa6734aa16c10
Dayane <dayanebmartins@gmail.com>
2024-03-20 17:27:31 -0300

    Arquivos movidos para a pasta app/backend

--- src/models/loginModels.js
+++
@@ -1,9 +0,0 @@
-const connection = require('./connection');
-
-const loginUser = async (email) => {
-  const query = 'SELECT * FROM pet_shop.users WHERE email=?';
-  const [[user]] = await connection.execute(query, [email]);
-  return user;
-};
-
-module.exports = { loginUser };
--- src/swagger.js
+++
@@ -1,9 +0,0 @@
-const swaggerAutogen = require('swagger-autogen')();
-
-const app = require('./app');
-
-const outputFile = './swagger_output.json';
-// terá o arquivo de configuração sobre nossas rotas que estão rodando no Express.
-const endpointsFiles = ['./routes/petsRoutes', './routes/donosRoutes'];
-// é o nosso caminho para as rotas a serem documentadas.
-swaggerAutogen(outputFile, endpointsFiles).then(() => app);
